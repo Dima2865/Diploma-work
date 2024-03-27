8afbd55dae7c29828a78c3e8c9bcfc86638d120c
Dima2865 <100437197+Dima2865@users.noreply.github.com>
2024-03-27 18:59:49 +0300

    Add files via upload

--- Yolo/YoloV8/main.py
+++ Yolo/YoloV8/main.py
@@ -4,7 +4,7 @@ import torchvision
 
 
 # pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-# yolo predict model=runs/detect/train/weights/best.pt source='D:\Programs\object_detection.v2i.yolov8\test\images\IMG_20240304_140604_jpg.rf.129ed14a8c99b3029701670e82cba98c.jpg' imgsz=640
+# yolo predict model=runs/detect/train2/weights/best.pt source='obj_detect.v1i.yolov8/test/images/IMG_20240327_145633_jpg.rf.0891fc27d71bedc8953af7c2e19603af.jpg' imgsz=640
 
 # Create a new YOLO model from scratch
 model = YOLO('yolov8n.yaml')
@@ -13,13 +13,13 @@ model = YOLO('yolov8n.yaml')
 model = YOLO('yolov8n.pt')
 
 # Train the model
-results = model.train(data='D:\Programs\YoloV8\data.yaml', epochs=50, imgsz=640)
+results = model.train(data='D:\Programs\YoloV8\obj_detect.v1i.yolov8\data.yaml', epochs=80, imgsz=640)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
 
 # Perform object detection on an image using the model
-results = model('D:\Programs\object_detection.v2i.yolov8\IMG_20240304_135054_jpg.rf.7beaf759b321d66b69feb353a6d644f1.jpg')
+results = model('D:/Programs/YoloV8/obj_detect.v1i.yolov8/test/images/IMG_20240327_144755_jpg.rf.99a56f506ec16ebffe197710f5a70ea1.jpg')
 
 # Export the model to ONNX format
 success = model.export(format='onnx')

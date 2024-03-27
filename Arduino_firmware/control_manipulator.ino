1b8d536c1717001f36f6b27dff21a00f96de07a3
Dima2865 <100437197+Dima2865@users.noreply.github.com>
2024-03-27 18:55:31 +0300

    Add files via upload

--- Arduino_firmware/control_manipulator.ino
+++ Arduino_firmware/control_manipulator.ino
@@ -24,29 +24,28 @@ void setup() {
   Serial.begin(9600);
 
   // назначение пинов платы для сервоприводов первого манипулятора 
-  servo1.attach(2, 500, 2400, 70); //основание
+  servo1.attach(2, 500, 2400, 87); //основание
   servo1.smoothStart();
-  servo1.setSpeed(40);
+  servo1.setSpeed(20);
   servo1.setAccel(0.08);
-  servo1.setMaxAngle(120);
 
   //поворачиваются против часовой от нуля градусов, по часовой к 180 (этикетка на меня)
-  servo2.attach(3, 500, 2400, 95); //1 плечо
+  servo2.attach(3, 500, 2400, 98); //1 плечо (5 - опущен параллельно, 90 - вртикально)
   servo2.smoothStart();
-  servo2.setSpeed(40);
+  servo2.setSpeed(50);
   servo2.setAccel(0.08);
 
-  servo3.attach(4, 500, 2400, 170); //2 плечо
+  servo3.attach(4, 500, 2400, 175); //2 плечо (175 - прижат, уменьшение)
   servo3.smoothStart();
-  servo3.setSpeed(40);
-  servo3.setAccel(0.08);
+  servo3.setSpeed(50);
+  servo3.setAccel(0.2);
 
-  servo4.attach(5, 500, 2400, 178); //3 плечо
+  servo4.attach(5, 500, 2400, 178); //3 плечо (178 - прижат, уменьшение)
   servo4.smoothStart();
-  servo4.setSpeed(40);
-  servo4.setAccel(0.08);
+  servo4.setSpeed(60);
+  servo4.setAccel(0.1);
 
-  servo5.attach(6, 500, 2400, 90); //захват
+  servo5.attach(6, 500, 2400, 90); //захват (90 - закрыт полностью, 20 - открыт, уменьшение - открытие)
   servo5.smoothStart();
   servo5.setSpeed(40);
   servo5.setAccel(0.08);
@@ -56,38 +55,137 @@ void loop() {
   if (Serial.available() > 0) {
     serial_input = Serial.readStringUntil('\n');
   }
+
+  //serial_input = "home_position";
+  //serial_input = "open_claw";
+  //serial_input = "close_claw";
+  //serial_input = "position_down_right";
+  //serial_input = "position_down_left";
+  //serial_input = "position_top_right";
+  //serial_input = "position_top_left";
+
+  if(serial_input == "home_position") {
+    home_position();
+  }
+
+  if(serial_input == "open_claw") {
+    open_claw();
+  }
+
+  if(serial_input == "close_claw") {
+    close_claw();
+  }
+
+  if(serial_input == "position_down_right") {
+    position_down_right();
+  }
+
+  if(serial_input == "position_top_right") {
+    position_top_right();
+  }
+
+  if(serial_input == "position_down_left") {
+    position_down_left();
+  }
+
+  if(serial_input == "position_top_left") {
+    position_top_left();
+  }
   
-  if(serial_input == "position_1") {
-    position_1();
-  }
 
-  if(serial_input == "position_2") {
-    position_2();
-  }
 }
 
-void position_1() {
-  //servo1.tick();
-  //servo1.setTargetDeg(50);
+
+void home_position() {
+  servo1.tick();
+  servo1.setTargetDeg(87);
+
   servo2.tick();
-  servo2.setTargetDeg(20);
+  servo2.setTargetDeg(98);
+  //servo2.write(98);
+
   servo3.tick();
-  servo3.setTargetDeg(85);
+  servo3.setTargetDeg(175);
+
   servo4.tick();
-  servo4.setTargetDeg(120);
-  servo5.tick();
-  servo5.setTargetDeg(30);
+  servo4.setTargetDeg(178);
 }
 
-void position_2() {
-  //servo1.tick();
-  //servo1.setTargetDeg(50);
-  servo2.tick();
-  servo2.setTargetDeg(80);
-  servo3.tick();
-  servo3.setTargetDeg(150);
-  servo4.tick();
-  servo4.setTargetDeg(120);
+
+void open_claw() {
   servo5.tick();
-  servo5.setTargetDeg(30);
+  servo5.setTargetDeg(20);
 }
+
+
+void close_claw() {
+  servo5.tick();
+  servo5.setTargetDeg(64);
+}
+
+
+void position_down_right() {
+  servo1.tick();
+  servo1.setTargetDeg(55);
+
+  servo2.tick();
+  servo2.setTargetDeg(32);
+  //servo2.write(40);
+
+  servo3.tick();
+  servo3.setTargetDeg(120);
+
+  servo4.tick();
+  servo4.setTargetDeg(150);
+}
+
+
+void position_down_left() {
+  servo1.tick();
+  servo1.setTargetDeg(87);
+
+  servo2.tick();
+  servo2.setTargetDeg(30);
+  //servo2.write(40);
+
+  servo3.tick();
+  servo3.setTargetDeg(140);
+
+  servo4.tick();
+  servo4.setTargetDeg(165);
+}
+
+
+void position_top_right() {
+  servo1.tick();
+  servo1.setTargetDeg(68);
+
+  servo2.tick();
+  servo2.setTargetDeg(33);
+  //servo2.write(39);
+
+  servo3.tick();
+  servo3.setTargetDeg(60);
+
+  servo4.tick();
+  servo4.setTargetDeg(95);
+}
+
+
+void position_top_left() {
+  servo1.tick();
+  servo1.setTargetDeg(87);
+  
+  servo2.tick();
+  servo2.setTargetDeg(33);
+  //servo2.write(39);
+
+  servo3.tick();
+  servo3.setTargetDeg(60);
+
+  servo4.tick();
+  servo4.setTargetDeg(95);
+}
+
+
+

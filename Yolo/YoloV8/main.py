4499bc089470ff2489da85a9daff2af87a0b62df
Dima2865 <100437197+Dima2865@users.noreply.github.com>
2024-06-07 09:30:52 +0300

    Add files via upload

--- Yolo/YoloV8/main.py
+++ Yolo/YoloV8/main.py
@@ -7,19 +7,23 @@ import torchvision
 # yolo predict model=runs/detect/train2/weights/best.pt source='obj_detect.v1i.yolov8/test/images/IMG_20240327_145633_jpg.rf.0891fc27d71bedc8953af7c2e19603af.jpg' imgsz=640
 
 # Create a new YOLO model from scratch
-model = YOLO('yolov8n.yaml')
+model = YOLO('yolov8s.yaml')
 
 # Load a pretrained YOLO model (recommended for training)
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8s.pt')
 
-# Train the model
-results = model.train(data='D:\Programs\YoloV8\obj_detect.v1i.yolov8\data.yaml', epochs=80, imgsz=640)
+# Train the model (было 200 эпох и 8 батч)
+results = model.train(data='D:\Programs\YoloV8\obj_detect.v1i.yolov8\data.yaml', epochs=100, batch=8, imgsz=640, pretrained=False)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
 
 # Perform object detection on an image using the model
-results = model('D:/Programs/YoloV8/obj_detect.v1i.yolov8/test/images/IMG_20240327_144755_jpg.rf.99a56f506ec16ebffe197710f5a70ea1.jpg')
+results = model('obj_detect.v2i.yolov8/test/images/IMG_20240327_145633_jpg.rf.0891fc27d71bedc8953af7c2e19603af.jpg')
+model.predict('obj_detect.v2i.yolov8/test/images/IMG_20240327_145633_jpg.rf.0891fc27d71bedc8953af7c2e19603af.jpg', save=True, imgsz=640, conf=0.5)
 
 # Export the model to ONNX format
 success = model.export(format='onnx')
+
+
+model = YOLO('runs/detect/train3/weights/best.pt')
---
+++ Yolo/YoloV8/real_time_detect_on_video.py
@@ -0,0 +1,62 @@
+import cv2
+from ultralytics import YOLO
+import re
+
+
+# Load the YOLOv8 model
+model = YOLO('runs/detect/train4/weights/best.pt')
+
+cap = cv2.VideoCapture(0)
+
+found_substring = ""
+objects_classes = ['obj_1_blue', 'obj_1_green', 'obj_1_red', 'obj_1_yellow', 'obj_2_blue', 'obj_2_green', 'obj_2_red', 'obj_2_yellow', 'obj_3_blue', 'obj_3_green', 'obj_3_red', 'obj_3_yellow']
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 inference on the frame
+        results = model(frame)
+
+        for r in results:
+            pattern = r"tensor\(\[[\d., ]+\], device='cuda:\d+'\)"
+            objects_info = re.search(pattern, str(r.boxes))
+
+            if objects_info:
+                found_substring = objects_info.group()
+                print("Найденная подстрока:", found_substring)
+            else:
+                print("Подстрока не найдена в выводе")
+
+        if "0." in found_substring or "4." in found_substring or "8." in found_substring:
+            print("blue")
+
+        if "1." in found_substring or "5." in found_substring or "9." in found_substring:
+            print("green")
+
+        if "2." in found_substring or "6." in found_substring or "10." in found_substring:
+            print("red")
+
+        if "3." in found_substring or "7." in found_substring or "11." in found_substring:
+            print("yellow")
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
+
+
